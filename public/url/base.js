fcf0dbe21c3fb62ddc0fc4df0edac20fa7d25615
Ananda Máyra <147098318+anandamayra1506@users.noreply.github.com>
2024-08-12 20:50:21 -0300

    base.js

--- public/url/base.js
+++ public/url/base.js
@@ -1,5 +1,5 @@
-const urlBaseAPI = 'https://gestao-locadora.onrender.com';
-const urlBaseFront = 'https://gestao-locadora.onrender.com';
+const urlBaseAPI = 'http://localhost:3000';
+const urlBaseFront = 'http://localhost:3000';
 
 // const urlBaseAPI = 'http://localhost:3000';
 // const urlBaseFront = 'http://127.0.0.1:5500/atividade8/back-node-fk-mono';
