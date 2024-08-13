96500c09aba99fc1a83733a0182946e8560fd806
Ananda Máyra <147098318+anandamayra1506@users.noreply.github.com>
2024-08-13 10:00:33 -0300

    events.js

--- public/registro/events.js
+++ public/registro/events.js
@@ -3,5 +3,6 @@ import { realizaLogin } from './registro.js';
 const btLogin = document.querySelector('form button');
 btLogin.addEventListener('click', (e) => {
     e.preventDefault();
+    realizaRegistro();
     realizaLogin();
 });
