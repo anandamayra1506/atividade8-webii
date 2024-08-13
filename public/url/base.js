b02e86af60755688044bd74c5b9a82f5e61e6494
Ananda Máyra <147098318+anandamayra1506@users.noreply.github.com>
2024-08-13 09:46:42 -0300

    base.js

--- public/url/base.js
+++ public/url/base.js
@@ -1,10 +1,4 @@
 const urlBaseAPI = 'http://localhost:3000';
 const urlBaseFront = 'http://localhost:3000';
 
-// const urlBaseAPI = 'http://localhost:3000';
-// const urlBaseFront = 'http://127.0.0.1:5500/atividade8/back-node-fk-mono';
-
-// 127.0.0.1:5500
-//http://127.0.0.1:5500/atividade8/back-node-fk-mono
-
 export { urlBaseAPI, urlBaseFront };
